a271032477a51fe970b626130072faf2e304b00f
Gigz <gigzpumpking@gmail.com>
2024-11-15 12:44:36 -0800

    Added visual feedback for selecting and dropping objects

--- Assets/Scripts/ShapeMover.cs
+++ Assets/Scripts/ShapeMover.cs
@@ -5,6 +5,8 @@ public class ShapeMover : MonoBehaviour
 {
     private GameObject selectedShape;
 
+    private GameObject highlightedShape;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -14,6 +16,9 @@ public class ShapeMover : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        RaycastMousePosition();
+
         if (Input.GetMouseButtonDown(0))
         {
             RaycastHit hit;
@@ -23,39 +28,52 @@ public class ShapeMover : MonoBehaviour
                 if (selectedShape == null)
                 {
                     selectedShape = hit.collider.gameObject;
+
+                    shapeState(false);
+
                     selectedShape.SetActive(false);
                 }
                 else
                 {
-                    //Debug.Log("New target found" + hit.collider.gameObject.transform.parent);
+                    selectedShape.transform.position = returnNewShapePosition(hit);
 
-                    // Calculate the new position based on the hit normal
-                    Vector3 newShapePosition = hit.collider.gameObject.transform.position + returnNewPosition(hit.normal);
-                    Debug.Log("Hit normal: " + hit.normal);
-
-                    Collider[] overlaps = Physics.OverlapBox(newShapePosition, new Vector3(0.5f, 0.5f, 0.5f));
-                    while (overlaps.Length > 0)
-                    {
-                        //Debug.Log("Cannot place shape here, overlaps detected with:");
-                        foreach (Collider col in overlaps)
-                        {
-                            //Debug.Log("Overlap with: " + col.gameObject.name);
-                        }
-                        // Adjust the position and check for overlaps again
-                        //Debug.Log("Attempted position: " + newShapePosition);
-                        newShapePosition += returnNewPosition(hit.normal);
-                        //Debug.Log("Updating new position: " + newShapePosition);
-                        overlaps = Physics.OverlapBox(newShapePosition, new Vector3(0.5f, 0.5f, 0.5f));
-                    }
-
-                    selectedShape.transform.position = newShapePosition;
-                    //Debug.Log("Shape placed at: " + newShapePosition);
+                    shapeState(true);
 
                     selectedShape.SetActive(true);
                     selectedShape = null;
                 }
             }
         }
+    }
+
+    void shapeState(bool state)
+    {
+        if (selectedShape == null) return;
+
+        if (state) {
+
+            // Set collider to true to allow raycast to hit the shape and player to walk on it
+            selectedShape.GetComponent<Collider>().enabled = true;
+        } else {
+
+            // Set collider to false to prevent raycast from hitting the shape and player from walking on it
+            selectedShape.GetComponent<Collider>().enabled = false;
+        }
+    }
+
+    Vector3 returnNewShapePosition(RaycastHit hit)
+    {
+        // Calculate the new position based on the hit normal
+        Vector3 newShapePosition = hit.collider.gameObject.transform.position + returnNewPosition(hit.normal);
+
+        Collider[] overlaps = Physics.OverlapBox(newShapePosition, new Vector3(0.5f, 0.5f, 0.5f));
+        while (overlaps.Length > 0)
+        {
+            newShapePosition += returnNewPosition(hit.normal);
+            overlaps = Physics.OverlapBox(newShapePosition, new Vector3(0.5f, 0.5f, 0.5f));
+        }
+
+        return newShapePosition;
     }
 
     Vector3 returnNewPosition(Vector3 hitNormal)
@@ -70,5 +88,78 @@ public class ShapeMover : MonoBehaviour
             hitNormal.z = hitNormal.z * 1.7f; 
         }
         return hitNormal;
+    }
+
+    void RaycastMousePosition()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (selectedShape == null)
+        {
+            HighlightShape(ray);
+        } else {
+            HighlightSpace(ray);
+        }
+
+
+    }
+
+    void HighlightShape(Ray ray) {
+        RaycastHit hit;
+
+        // If the ray hits a shape, highlight it
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            if (hit.collider.gameObject.tag == "Shape")
+            {
+                if (highlightedShape != null)
+                {
+                    highlightedShape.GetComponent<Renderer>().material.color = Color.white;
+                }
+
+                highlightedShape = hit.collider.gameObject;
+                highlightedShape.GetComponent<Renderer>().material.color = Color.yellow;
+            }
+            else
+            {
+                if (highlightedShape != null)
+                {
+                    highlightedShape.GetComponent<Renderer>().material.color = Color.white;
+                    highlightedShape = null;
+                }
+            }
+        }
+        else
+        {
+            if (highlightedShape != null)
+            {
+                highlightedShape.GetComponent<Renderer>().material.color = Color.white;
+                highlightedShape = null;
+            }
+        }
+    }
+
+    void HighlightSpace(Ray ray) {
+        // Highlights the location where the shape will be placed by placing the unactivated cube at the location
+        
+        RaycastHit hit;
+
+        bool isHit = Physics.Raycast(ray, out hit);
+
+        if (isHit && (hit.collider.gameObject == selectedShape)) {
+            return;
+        }
+
+        if (isHit) {
+            Vector3 newShapePosition = returnNewShapePosition(hit);
+
+            selectedShape.transform.position = newShapePosition;
+
+            selectedShape.SetActive(true);
+        } else {
+            selectedShape.SetActive(false);
+        }
+
     }
 }
