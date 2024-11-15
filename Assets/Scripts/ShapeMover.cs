7a646896a17b021541894ec2f60c5c332ccdeb72
Rmojarro1 <48000819+Rmojarro1@users.noreply.github.com>
2024-11-15 11:59:43 -0800

    Fixed gap issue between blocks

--- Assets/Scripts/ShapeMover.cs
+++ Assets/Scripts/ShapeMover.cs
@@ -27,31 +27,48 @@ public class ShapeMover : MonoBehaviour
                 }
                 else
                 {
-                    Debug.Log("New target found" + hit.collider.gameObject.transform.parent);
+                    //Debug.Log("New target found" + hit.collider.gameObject.transform.parent);
 
                     // Calculate the new position based on the hit normal
-                    Vector3 newShapePosition = hit.collider.gameObject.transform.position + hit.normal;
+                    Vector3 newShapePosition = hit.collider.gameObject.transform.position + returnNewPosition(hit.normal);
+                    Debug.Log("Hit normal: " + hit.normal);
 
                     Collider[] overlaps = Physics.OverlapBox(newShapePosition, new Vector3(0.5f, 0.5f, 0.5f));
                     while (overlaps.Length > 0)
                     {
-                        Debug.Log("Cannot place shape here, overlaps detected with:");
+                        //Debug.Log("Cannot place shape here, overlaps detected with:");
                         foreach (Collider col in overlaps)
                         {
-                            Debug.Log("Overlap with: " + col.gameObject.name);
+                            //Debug.Log("Overlap with: " + col.gameObject.name);
                         }
                         // Adjust the position and check for overlaps again
-                        newShapePosition += hit.normal;
+                        //Debug.Log("Attempted position: " + newShapePosition);
+                        newShapePosition += returnNewPosition(hit.normal);
+                        //Debug.Log("Updating new position: " + newShapePosition);
                         overlaps = Physics.OverlapBox(newShapePosition, new Vector3(0.5f, 0.5f, 0.5f));
                     }
 
                     selectedShape.transform.position = newShapePosition;
-                    Debug.Log("Shape placed at: " + newShapePosition);
+                    //Debug.Log("Shape placed at: " + newShapePosition);
 
                     selectedShape.SetActive(true);
                     selectedShape = null;
                 }
             }
         }
+    }
+
+    Vector3 returnNewPosition(Vector3 hitNormal)
+    {
+        if(hitNormal.x > 0){
+            hitNormal.x = hitNormal.x * 1.7f; 
+        }
+        else if(hitNormal.y > 0){
+            hitNormal.y = hitNormal.y * 1.7f; 
+        }
+        else if(hitNormal.z > 0){
+            hitNormal.z = hitNormal.z * 1.7f; 
+        }
+        return hitNormal;
     }
 }
