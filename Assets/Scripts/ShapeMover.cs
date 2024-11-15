c37bf04d3166b3e227bd0e47c33fb12899fde66e
Mako1688 <128003121+Mako1688@users.noreply.github.com>
2024-11-14 21:48:28 -0800

    Made block editing got to the side clicked

--- Assets/Scripts/ShapeMover.cs
+++ Assets/Scripts/ShapeMover.cs
@@ -3,41 +3,37 @@ using UnityEngine;
 
 public class ShapeMover : MonoBehaviour
 {
-    
-    private GameObject selectedShape; 
-    
+    private GameObject selectedShape;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        selectedShape = null; 
+        selectedShape = null;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0))
         {
-            RaycastHit hit; 
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition); 
-            if(Physics.Raycast(ray, out hit))
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit))
             {
-                if(selectedShape == null)
+                if (selectedShape == null)
                 {
-                    selectedShape = hit.collider.gameObject; 
-                    selectedShape.SetActive(false); 
+                    selectedShape = hit.collider.gameObject;
+                    selectedShape.SetActive(false);
                 }
                 else
                 {
-                    Debug.Log("New target found"+ hit.collider.gameObject.transform.parent);
-                    Vector3 newShapePosition = hit.collider.gameObject.transform.position + new Vector3(0, 0, 1.7f); 
-                    /*if(Physics.OverlapBox(newShapePosition, new Vector3(0.5f, 0.5f, 0.5f)).Length > 1)
-                    {
-                        Debug.Log("Cannot place shape here");
-                        newShapePosition = selectedShape.transform.position + new Vector3(0, 0, 1.7f);
-                        selectedShape.transform.position = newShapePosition;
-                    }*/
+                    Debug.Log("New target found" + hit.collider.gameObject.transform.parent);
+
+                    // Calculate the new position based on the hit normal
+                    Vector3 newShapePosition = hit.collider.gameObject.transform.position + hit.normal;
+
                     Collider[] overlaps = Physics.OverlapBox(newShapePosition, new Vector3(0.5f, 0.5f, 0.5f));
-                    while (overlaps.Length > 0) 
+                    while (overlaps.Length > 0)
                     {
                         Debug.Log("Cannot place shape here, overlaps detected with:");
                         foreach (Collider col in overlaps)
@@ -45,14 +41,13 @@ public class ShapeMover : MonoBehaviour
                             Debug.Log("Overlap with: " + col.gameObject.name);
                         }
                         // Adjust the position and check for overlaps again
-                        newShapePosition += new Vector3(0, 0, 1.7f);
+                        newShapePosition += hit.normal;
                         overlaps = Physics.OverlapBox(newShapePosition, new Vector3(0.5f, 0.5f, 0.5f));
-                    } 
-                    
-                        selectedShape.transform.position = newShapePosition;
-                        Debug.Log("Shape placed at: "+ newShapePosition);
-                    
-                    
+                    }
+
+                    selectedShape.transform.position = newShapePosition;
+                    Debug.Log("Shape placed at: " + newShapePosition);
+
                     selectedShape.SetActive(true);
                     selectedShape = null;
                 }
